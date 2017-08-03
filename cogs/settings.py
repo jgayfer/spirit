442e85a24ddbfc39679153fbe83b70cc27ff57de
James Gayfer <gayfer.james@gmail.com>
2017-08-02 17:05:28 -0700

    setprefix can now be used in a dm

--- cogs/settings.py
+++ cogs/settings.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from db.dbase import DBase
 import discord
 
-from cogs.utils.messages import MessageManager
+from cogs.utils.messages import MessageManager, get_server_from_dm
+from cogs.utils.checks import is_admin
 
 
 class Settings:
@@ -13,24 +14,31 @@ class Settings:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def setprefix(self, ctx, new_prefix):
+    async def setprefix(self, ctx, new_prefix=None):
 
         user = ctx.message.author
         manager = MessageManager(self.bot, user, ctx.message.channel, ctx.message)
 
-        if not user.server_permissions.administrator:
-            await manager.say("Oops! You must be an admin to do that.")
+        server = None
+        if ctx.message.channel.is_private:
+            server = get_server_from_dm(self.bot, ctx)
+            if not server:
+                return await manager.say("You must be part of only one {} server to do that in a DM".format(self.bot.user.mention))
+        else:
+            server = ctx.message.server
+
+        if not is_admin(user, server):
+            await manager.say("You must be an admin to do that.")
             return await manager.clear()
 
-        # Return if the user is in a private message as prefixes are server specific
-        if ctx.message.channel.is_private:
-            return await manager.say("That command is not supported in a direct message.")
+        if not new_prefix:
+            await manager.say("You must provide a new prefix.")
 
         if len(new_prefix) > 5:
-            await manager.say("Oops! Prefix must be less than 6 characters")
+            await manager.say("Oops! Prefix must be less than 6 characters.")
             return await manager.clear()
 
         with DBase() as db:
-            db.set_prefix(ctx.message.server.id, new_prefix)
+            db.set_prefix(server.id, new_prefix)
             await manager.say("Command prefix has been changed to " + new_prefix)
             return await manager.clear()
