0551689b052536a5708790d9359fc6a0f63c8ee9
James Gayfer <gayfer.james@gmail.com>
2017-10-02 16:39:40 -0700

    fix typo

--- cogs/destiny.py
+++ cogs/destiny.py
@@ -81,7 +81,7 @@ class Destiny:
             await manager.say("Invalid account name. If this seems wrong, please contact the developer.")
             return await manager.clear()
         except pydest.PydestException as e:
-            await manager.say("I can seem to connect to Bungie right now. Try again later.")
+            await manager.say("I can't seem to connect to Bungie right now. Try again later.")
             return await manager.clear()
 
         act_exists = False
